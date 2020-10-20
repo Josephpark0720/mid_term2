76203f8e54d98a55c44cda0f23d088ebd2c7bc20
Josephpark0720 <70733078+Josephpark0720@users.noreply.github.com>
2020-10-21 06:55:35 +0900

    1

--- app.py
+++ app.py
@@ -1,22 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
+from wtforms import Form, StringField, TextAreaField
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'random'
 bootstrap = Bootstrap(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Format(Form):
+    title = StringField('Title')
+    name = StringField('Name')
+    content = TextAreaField('Content')
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-    title = "こんにちは"
+@app.route('/form', methods=['GET', 'POST'])
+def form():
     if request.method == 'POST':
         name = request.form['name']
         return redirect(url_for('home'))
     else:
-        return render_template('index.html')
+        return render_template('form.html')
 
 if __name__ == '__main__':
     app.debug = True
