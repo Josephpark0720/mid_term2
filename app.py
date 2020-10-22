be47aefcf79eefa2de427f8700a6d1eac134a662
Josephpark0720 <70733078+Josephpark0720@users.noreply.github.com>
2020-10-22 21:16:10 +0900

    final

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
-from wtforms import Form, StringField, TextAreaField
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap, bootstrap_find_resource
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+bootstrap = Bootstrap(app)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'random'
-bootstrap = Bootstrap(app)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
@@ -17,20 +17,17 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    title = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(80), unique=True)
+    content = db.Column(db.String(300), unique=True)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    def __init__(self, title, name, content):
+        self.title = title
+        self.name = name
+        self.content = content
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
-class Format(Form):
-    title = StringField('Title')
-    name = StringField('Name')
-    content = TextAreaField('Content')
+        return "User<{}, {}, {}>".format(self.title, self.name, self.content)
 
 @app.route('/')
 def home():
